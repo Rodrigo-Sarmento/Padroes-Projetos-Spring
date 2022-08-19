3091e0e12f030522903fa5dcd26fce982953ceec
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2022-08-19 11:32:42 -0300

    inicial

---
+++ src/test/java/digo/gof/PadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package digo.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

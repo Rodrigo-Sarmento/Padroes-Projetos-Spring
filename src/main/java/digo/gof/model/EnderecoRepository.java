065b15a49e785d55c5160df9146c7d94ad649fba
Rodrigo Sarmento <rodrigogomessarmento@gmail.com>
2022-08-19 11:47:45 -0300

    add: endereco model

--- src/main/java/digo/gof/model/EnderecoRepository.java
+++ src/main/java/digo/gof/model/EnderecoRepository.java
@@ -1,4 +1,9 @@
 package digo.gof.model;
 
-public class EnderecoRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+
 }
